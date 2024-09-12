9e7392755ed07113a81ac00b4121f47f355c40be
Netibox <aszsave123@gmail.com>
2024-09-12 14:54:46 +0700

    Update muneall.js

--- js/muneall.js
+++ js/muneall.js
@@ -11,7 +11,7 @@ function kids() {
   location.href = "go:kids";
 };
 function TVonline() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://github.com/Netibox/wep1idtv/blob/main/Mune/indextvmov.html#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=MenuAll;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+  document.getElementById("objek").innerHTML = '<a href="https://github.com/Netibox/Netbox2/blob/main/index.html/" alt="Mythrill" target="_blank">'
 };
 function sports() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://github.com/Netibox/Netbox2/blob/main/index.html#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=MenuAll;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
