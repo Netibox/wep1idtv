a62e0f23b55aa177b035cdc9a2b54919b187a573
Netibox <aszsave123@gmail.com>
2024-09-12 18:59:45 +0700

    Update tvonline.js

--- js/tvonline.js
+++ js/tvonline.js
@@ -1,15 +1,4 @@
-function tv3hd() {
-  location.href = "go:tv3hd";
-};
-function true4u() {
-  location.href = "go:true4u";
-};
-function tv7hd() {
-  location.href = "go:tv7hd";
-};
-function ch8() {
-  location.href = "go:ch8";
-};
+
 function tv3hd() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://www.inwiptv.com/player_demo.php?channel=27137|referer=https://inwiptv.com/&User-Agent=Mozilla/5.0%20(Linux;%20Android%2011;%20SM-A307GN%20Build/RP1A.200720.012;%20wv)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Version/4.0%20Chrome/107.0.5304.91%20Mobile%20Safari/537.36%20Vinebre#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=TVNETBOX;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
