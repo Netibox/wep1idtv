194dca7c76e1daabdff95623a9c00eeb5924bde1
Netibox <aszsave123@gmail.com>
2024-09-12 14:44:42 +0700

    Add files via upload

---
+++ js/muneall.js
@@ -0,0 +1,32 @@
+function TVonline() {
+  location.href = "go:TVonline";
+};
+function sports() {
+  location.href = "go:sports";
+};
+function movietv() {
+  location.href = "go:movietv";
+};
+function kids() {
+  location.href = "go:kids";
+};
+function TVonline() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://github.com/Netibox/wep1idtv/blob/main/Mune/indextvmov.html#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=MenuAll;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+};
+function sports() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://github.com/Netibox/Netbox2/blob/main/index.html#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=MenuAll;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+};
+function movietv() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://github.com/Netibox/netboxs/blob/main/index.html#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=MenuAll;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+};
+function kids() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://github.com/Netibox/Netbox/blob/main/index.html#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=MenuAll;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+};
+function sarakdee() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://github.com/Netibox/Netbox/blob/main/index.html#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=MenuAll;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+};
+function chat() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://github.com/Netibox/wep1idtv/blob/main/Mune/indextvmov.html#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=MenuAll;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+};
+
+
