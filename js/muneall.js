f3a0aa381bc3440982a57fd6c168e17de38a2174
Netibox <aszsave123@gmail.com>
2024-09-12 15:01:04 +0700

    Update muneall.js

--- js/muneall.js
+++ js/muneall.js
@@ -11,22 +11,22 @@ function kids() {
   location.href = "go:kids";
 };
 function TVonline() {
-  document.getElementById("objek").innerHTML = '<a href="https://github.com/Netibox/Netbox2/blob/main/index.html/" alt="Mythrill" target="_blank">'
+  document.getElementById("objek").innerHTML = '<iframe src="intent:https://github.com/Netibox/wep1idtv/blob/main/Mune/indextvmov.html#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=MenuAll;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function sports() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://github.com/Netibox/Netbox2/blob/main/index.html#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=MenuAll;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+  document.getElementById("objek").innerHTML = '<iframe src="intent:https://github.com/Netibox/Netbox2/blob/main/index.html#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=MenuAll;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function movietv() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://github.com/Netibox/netboxs/blob/main/index.html#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=MenuAll;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+  document.getElementById("objek").innerHTML = '<iframe src="intent:https://github.com/Netibox/netboxs/blob/main/index.html#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=MenuAll;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function kids() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://github.com/Netibox/Netbox/blob/main/index.html#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=MenuAll;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+  document.getElementById("objek").innerHTML = '<iframe src="intent:https://github.com/Netibox/Netbox/blob/main/index.html#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=MenuAll;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function sarakdee() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://github.com/Netibox/Netbox/blob/main/index.html#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=MenuAll;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+  document.getElementById("objek").innerHTML = '<iframe src="intent:https://github.com/Netibox/Netbox/blob/main/index.html#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=MenuAll;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function chat() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://github.com/Netibox/wep1idtv/blob/main/Mune/indextvmov.html#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=MenuAll;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+  document.getElementById("objek").innerHTML = '<iframe src="intent:https://github.com/Netibox/wep1idtv/blob/main/Mune/indextvmov.html#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=MenuAll;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 
 
