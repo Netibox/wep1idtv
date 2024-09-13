564a7efa55cdc844719adab7052e169f690f2fd9
Netibox <aszsave123@gmail.com>
2024-09-13 12:51:03 +0700

    Update tvonline.js

--- Mune/js/tvonline.js
+++ Mune/js/tvonline.js
@@ -1,17 +1,17 @@
 
-function tv3hd() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://www.livedoomovies.com:4431/02_5HD_720p/chunklist.m3u8#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=TVNETBOX;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+function tvhd3() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://ch3-33-web.cdn.byteark.com/live/playlist_720p/index.m3u8#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=TVNETBOX;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function tv5hd() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://www.livedoomovies.com:4431/02_5HD_720p/chunklist.m3u8#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=TVNETBOX;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
-function tv7hd() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://www.livedoomovies.com:4431/02_5HD_720p/chunklist.m3u8#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=TVNETBOX;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+function tvhd7() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://live-cdn-hwc.ch7.com/livech7hd/HD_720p.m3u8#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=TVNETBOX;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function thairat() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://edge6a.v2h-cdn.com/thairathhd/thairathHD.stream/playlist.m3u8#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=TVNETBOX;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
-function true4u() {
+function true4utv() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://edge6a.v2h-cdn.com/true4u/true4u.stream/playlist.m3u8#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=TVNETBOX;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function amaran() {
@@ -20,8 +20,8 @@ function amaran() {
 function gmmz() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://dookeela.live/live-tv/gmm25#Intent;package=com.wiseplay;S.referer=https://dookeela.live/&userAgent=exo_player;S.title=TVonline;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
-function ch8() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://www.inwiptv.com/player_demo.php?channel=27147#Intent;package=com.wiseplay;S.referer=https://www.inwiptv.com/&userAgent=exo_player;S.title=TVonline;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+function ch8hd() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://www.inwiptv.com/player_demo.php?channel=27147#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=TVNETBOX;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function mono() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://www.adintrend.tv/hd/m/ch38#Intent;package=com.wiseplay;S.referer=https://www.adintrend.tv/&userAgent=exo_player;S.title=TVonline;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
@@ -32,6 +32,6 @@ function workpoint() {
 function pptv() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://www.inwiptv.com/player_demo.php?channel=27129#Intent;package=com.wiseplay;S.referer=https://www.inwiptv.com/&userAgent=exo_player;S.title=TVonline;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
-function onehd() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://www.ufa77.fun/channel?ch=onehd#Intent;package=com.wiseplay;S.referer=https://www.ufa77.fun/&userAgent=exo_player;S.title=TVonline;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+function onehdtv() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://bcovlive-a.akamaihd.net/e11d0db446fe41fe9be61157033f9325/ap-southeast-1/6415628290001/playlist.m3u8#Intent;package=com.wiseplay;S.referer=https://bcovlive-a.akamaihd.net/&userAgent=exo_player;S.title=TVonline;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
