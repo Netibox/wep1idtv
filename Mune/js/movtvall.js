0ac73c337ac843d82353751e5dcdf34239427c58
Netibox <aszsave123@gmail.com>
2024-09-13 14:34:05 +0700

    Update movtvall.js

--- Mune/js/movtvall.js
+++ Mune/js/movtvall.js
@@ -63,5 +63,5 @@ function mvtv() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://edge6a.v2h-cdn.com/mvtv75/mvtv75.stream/chunklist_w640027843.m3u8#Intent;package=com.wiseplay;S.referer=https://edge6a.v2h-cdn.com/&userAgent=exo_player;S.title=TVonline;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="MOVIETV"></iframe>'
 };
 function attv() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://49.0.87.24:1936/HDAttv/Attv/playlist.m3u8#Intent;package=com.wiseplay;S.referer=https://49.0.87.24:1936/HDAttv/Attv/&userAgent=exo_player;S.title=TVonline;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="MOVIETV"></iframe>'
+  document.getElementById("objek").innerHTML = '<iframe src="intent://49.0.87.24:1936/HDAttv/Attv/playlist.m3u8|referer=https://49.0.87.24:1936/HDAttv/Attv/&User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36 Edg/102.0.1245.33#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=TVonline;end" height="0" width="0" style="border:none;" title="TVonline"></iframe>'
 };
