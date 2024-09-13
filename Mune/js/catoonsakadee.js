723b6640cd5ea07a88c6c96c648b81d7cae5941d
Netibox <aszsave123@gmail.com>
2024-09-13 12:59:03 +0700

    Update catoonsakadee.js

--- Mune/js/catoonsakadee.js
+++ Mune/js/catoonsakadee.js
@@ -1,15 +1,4 @@
-function tv3hd() {
-  location.href = "go:tv3hd";
-};
-function true4u() {
-  location.href = "go:true4u";
-};
-function tv7hd() {
-  location.href = "go:tv7hd";
-};
-function ch8() {
-  location.href = "go:ch8";
-};
+
 function toonclub() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://edge2-bkk.3bb.co.th:9443/Web_CartoonClub_Live/cartoonclub_480P.stream/chunklist_w1781446574.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9OC8yNy8yMDIzIDQ6MjA6MjEgQU0maGFzaF92YWx1ZT1YOUxyWGttVEg0cHRQV05TNVRnZEZRPT0mdmFsaWRtaW51dGVzPTI=#Intent;package=com.wiseplay;S.referer=https://edge2-bkk.3bb.co.th:9443/&userAgent=exo_player;S.title=CartoonClub;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
