b6c71ae9e0efaeb38fea309432fe7a1b9636a125
Netibox <aszsave123@gmail.com>
2024-09-13 18:05:51 +0700

    Update mune.js

--- js/mune.js
+++ js/mune.js
@@ -1,14 +1,17 @@
-function tv3hd() {
-  location.href = "go:tv3hd";
+function tvall() {
+  location.href = "Mune/alltvindex.html";
 };
-function true4u() {
-  location.href = "go:true4u";
+function sports() {
+  location.href = "https://dookeela.live/live-tv/spotv";
 };
-function tv7hd() {
-  location.href = "go:tv7hd";
+function kisss() {
+  location.href = "go:kisss";
 };
-function ch8() {
-  location.href = "go:ch8";
+function sacadee() {
+  location.href = "go:sacadee";
+};
+function chat() {
+  location.href = "go:chat";
 };
 function toonclub() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://edge2-bkk.3bb.co.th:9443/Web_CartoonClub_Live/cartoonclub_480P.stream/chunklist_w1781446574.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9OC8yNy8yMDIzIDQ6MjA6MjEgQU0maGFzaF92YWx1ZT1YOUxyWGttVEg0cHRQV05TNVRnZEZRPT0mdmFsaWRtaW51dGVzPTI=#Intent;package=com.wiseplay;S.referer=https://edge2-bkk.3bb.co.th:9443/&userAgent=exo_player;S.title=CartoonClub;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
@@ -27,25 +30,4 @@ function toonnetw() {
 };
 function fwtoon() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://www.inwiptv.com/player_demo.php?channel=89780#Intent;package=com.wiseplay;S.referer=https://www.inwiptv.com/&userAgent=exo_player;S.title=FWToon;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
-};
-function sparkplay() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://dookeela.live/live-tv/truesparkplay#Intent;package=com.wiseplay;S.referer=https://www.inwiptv.com/&userAgent=exo_player;S.title=TRUESPARKPLAY;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
-};
-function samrujlok() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://dookeela.live/live-tv/samrujlok#Intent;package=com.wiseplay;S.referer=https://dookeela.live/&userAgent=exo_player;S.title=sumlaoluk;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
-};
-function animal() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://dookeela.live/live-tv/animalshow#Intent;package=com.wiseplay;S.referer=https://dookeela.live/&userAgent=exo_player;S.title=AnimalShow;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
-};
-function mysci() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://dookeela.live/live-tv/mysci#Intent;package=com.wiseplay;S.referer=https://dookeela.live/&userAgent=exo_player;S.title=MYSCI;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
-};
-function discovery() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://streaming.livescorethai.net/iptv/hd-discoveryeng.stream/chunks.m3u8#Intent;package=com.wiseplay;S.referer=https://freetvdd.com/&userAgent=exo_player;S.title=DISCOVERY;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
-};
-function hitstory() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://streaming.livescorethai.net/iptv/hd-historyeng.stream/chunks.m3u8#Intent;package=com.wiseplay;S.referer=https://freetvdd.com/&userAgent=exo_player;S.title=HISTORYEN;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
-};
-function fwdoc() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://www.inwiptv.com/player_demo.php?channel=91303#Intent;package=com.wiseplay;S.userAgent=exo_player;S.title=FWDOC;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
