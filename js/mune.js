ea5ac499e74e835a1ce08433d2ee8cbd1f00b10b
Netibox <aszsave123@gmail.com>
2024-09-13 18:50:03 +0700

    Update mune.js

--- js/mune.js
+++ js/mune.js
@@ -1,10 +1,9 @@
 
 function sports() {
- window.location location.href = "https://www.heng99bet.com/channels";
+ window.location = "intent://streaming.livescorethai.net/iptv/epl-1.stream/playlist.m3u8#Intent;package=com.wiseplay;S.referer=https://freetvdd.com/&userAgent=exo_player;S.title=Sports;scheme=https;type=video/mp4;end";
 };
 function tvall() {
-  document.getElementById("objek").innerHTML = "W3Schools";
-  element.href = "https://dookeela.live/live-tv/spotv";
+  document.getElementById("objek").innerHTML = '<iframe src="intent://streaming.livescorethai.net/iptv/uk-cartoonito.stream/chunks.m3u8#Intent;package=com.wiseplay;S.referer=https://freetvdd.com/&userAgent=exo_player;S.title=CartoonClub;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function sports() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://streaming.livescorethai.net/iptv/uk-cartoonito.stream/chunks.m3u8#Intent;package=com.wiseplay;S.referer=https://freetvdd.com/&userAgent=exo_player;S.title=CartoonClub;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
