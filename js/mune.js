a4aafe1d2d3872600ac764643072a75ba9efd80e
Netibox <aszsave123@gmail.com>
2024-09-13 19:15:24 +0700

    Update mune.js

--- js/mune.js
+++ js/mune.js
@@ -1,9 +1,8 @@
-
 function sports() {
- window.location.href = "intent:https://www.xn--72czaud0ezbn4b8de.com/live/player/epl4#Intent;package=com.wiseplay;S.referer=https://freetvdd.com/&userAgent=exo_player;S.title=Sports;scheme=https;type=video/mp4;end";
+  document.getElementById("objek").innerHTML = '<iframe src="intent://www.livedoomovies.com:4431/02_5HD_720p/chunklist.m3u8#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=TVNETBOX;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function tvall() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://www.xn--72czaud0ezbn4b8de.com/live/player/epl4#Intent;package=com.wiseplay;S.referer=https://freetvdd.com/&userAgent=exo_player;S.title=CartoonClub;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+  document.getElementById("objek").innerHTML = '<iframe src="intent://www.livedoomovies.com:4431/02_5HD_720p/chunklist.m3u8#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=TVNETBOX;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function sports11() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://streaming.livescorethai.net/iptv/uk-cartoonito.stream/chunks.m3u8#Intent;package=com.wiseplay;S.referer=https://freetvdd.com/&userAgent=exo_player;S.title=CartoonClub;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
