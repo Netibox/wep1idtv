c374e0352554993d0f32d9cd0d30771b3ef5f3de
Netibox <aszsave123@gmail.com>
2024-09-13 21:04:17 +0700

    Update mune.js

--- js/mune.js
+++ js/mune.js
@@ -1,12 +1,8 @@
-function sports() {
-  document.getElementById("objek").innerHTML =  "W3Schools";
-  element.href = "https://dookeela.live/live-tv/spotv";
-};
 function tvall() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://dookeela.live/live-tv/#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=TVNETBOX;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
-function sports11() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://streaming.livescorethai.net/iptv/uk-cartoonito.stream/chunks.m3u8#Intent;package=com.wiseplay;S.referer=https://freetvdd.com/&userAgent=exo_player;S.title=CartoonClub;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+function sports() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn1.aws-live-streaming.com/ais/tsp2/playlist.m3u8|referer=https://aisplay24hd.com#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
 };
 function kisss() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://dookeela.live/live-tv/nickjr#Intent;package=com.wiseplay;S.referer=https://dookeela.live/&userAgent=exo_player;S.title=NICKJR;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
