929123af38dce55bda6fb8954648d428ab897cd2
Netibox <aszsave123@gmail.com>
2024-09-13 21:46:47 +0700

    Update mune.js

--- js/mune.js
+++ js/mune.js
@@ -1,21 +1,18 @@
-function hd() {
-  location.href = "go:hd";
-};
 function tvall() {
-  location.href = "Mune/movindex.html";
+  location.href = "Mune/alltvindex.html";
 };
 function sports() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent:https://dookeela.live/live-tv/#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=TVNETBOX;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+  location.href = "Mune/sspindex.html";
 };
-function sports1() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn1.aws-live-streaming.com/ais/tsp2/playlist.m3u8|referer=https://aisplay24hd.com#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+function movie() {
+  location.href = "Mune/movindex.html";
 };
 function kisss() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://dookeela.live/live-tv/nickjr#Intent;package=com.wiseplay;S.referer=https://dookeela.live/&userAgent=exo_player;S.title=NICKJR;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+  location.href = "Mune/toonindex.html";
 };
 function sacadee() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://dookeela.live/live-tv/boomerang#Intent;package=com.wiseplay;S.referer=https://dookeela.live/&userAgent=exo_player;S.title=BOOMERANG;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+  location.href = "Mune/sacadeeindex.html";
 };
 function chat() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://streaming.livescorethai.net/iptv/sd-cartoonnetwork.stream/chunks.m3u8#Intent;package=com.wiseplay;S.referer=https://freetvdd.com/&userAgent=exo_player;S.title=CARTOONNETWORK;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+  location.href = "Mune/chatindex.html";
 };
