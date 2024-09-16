574e0487e8491cec78a0a038c00aed6ebc7719cf
Netibox <aszsave123@gmail.com>
2024-09-16 14:41:44 +0700

    Update movtvall.js

--- Mune/js/movtvall.js
+++ Mune/js/movtvall.js
@@ -69,7 +69,7 @@ function hboa2() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://appletree-mytime-samsungbrazil.amagi.tv/playlist360p.m3u8#Intent;package=com.wiseplay;S.referer=https://appletree-mytime-samsungbrazil.amagi.tv/&userAgent=exo_player;S.title=TVonline;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="MOVIETV"></iframe>'
 };
 function attv1() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://49.0.87.24:1936/HDAttv/Attv/playlist.m3u8?network_id=535&User-Agent=Mozilla/5.0%20(Linux;%20Android%2011;%20SM-A307GN%20Build/RP1A.200720.012;%20wv)%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Version/4.0%20Chrome/107.0.5304.91%20Mobile%20Safari/537.36%20Vinebre#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=MovieTV;end" height="0" width="0" style="border:none;" title="MovieTV"></iframe>'
+  document.getElementById("objek").innerHTML = '<iframe src="intent://49.0.87.24:1936/HDAttv/Attv/playlist.m3u8#Intent;package=com.wiseplay;S.referer=https://49.0.87.24:1936/HDAttv/Attv/&userAgent=exo_player;S.title=TVonline;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="MOVIETV"></iframe>'
 };
 function mangorn() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://dookeela.live/live-tv/mangorn#Intent;package=com.wiseplay;S.referer=https://dookeela.live/&userAgent=exo_player;S.title=TVonline;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="MOVIETV"></iframe>'
@@ -84,5 +84,5 @@ function mpdcinemax() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://cdnjkt4.transvision.co.id:1000/live/master/3/4028c6856c3db2cc016cdbfc4a1934bf/manifest.mpd|drmScheme=widevine&drmLicense=https://lic-cubmux.konslet.workers.dev/4rr0w/play.wv#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=Movietv;end" height="0" width="0" style="border:none;" title="Movietv"></iframe>'
 };
 function mpdsignature() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://cdnjkt4.transvision.co.id:1000/live/master/3/4028c6856c3db2cc016d0552e0ca37a2/manifest.mpd|drmScheme=widevine&drmLicense=https://lic-cubmux.konslet.workers.dev/4rr0w/play.wv#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=NGTV;end" height="0" width="0" style="border:none;" title="SS Sport"></iframe>'
+  document.getElementById("objek").innerHTML = '<iframe src="intent://tglmp01.akamaized.net/out/v1/29687feb8f96427ca7348cd59c2338d9/manifest.mpd|drmScheme=clearkey&drmLicense=a407479f074d43d798478c1212d95876:3afb53a2643939cd7128fa7ceded533a#Intent;scheme=https;type=video/*;package=com.genuine.leone;S.browser_fallback_url=market://details?id=com.genuine.leone.ad;S.title=Movietv;end" height="0" width="0" style="border:none;" title="Movietv"></iframe>'
 };
