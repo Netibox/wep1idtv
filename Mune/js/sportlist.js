85254c3594011d8223f5e1154cb408dbad5da228
Netibox <aszsave123@gmail.com>
2024-09-20 13:59:28 +0700

    Add files via upload

---
+++ Mune/js/sportlist.js
@@ -0,0 +1,34 @@
+{
+  "sportlist": [
+    
+    {
+      "poster": "https://i.imgur.com/tsp44Cz.jpeg",
+      "url": "intent://main.24playerhd.com/newplaylist/2f41c01ce5601b8bd579b62a/2f41c01ce5601b8bd579b62a.m3u8#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=MOVIETV;end"
+    },
+       {
+      "poster": "https://s359.kapook.com/pagebuilder/542d8cc8-8129-4d8f-ac1f-a8a1747ff57b.jpg",
+      "url": "intent://main.24playerhd.com/newplaylist/2126ee71e8bb50cea592642a/2126ee71e8bb50cea592642a.m3u8#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=MOVIETV;end"
+    },
+ 
+    {
+      "poster": "https://www.heavenofhorror.com/wp-content/uploads/2024/09/Outlaw-Bandida-Netflix-Review-1200x900.jpg",
+      "url": "intent://main.24playerhd.com/newplaylist/2492527afd16cf717160e2ad/2492527afd16cf717160e2ad.m3u8#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=MOVIETV;end"
+    },
+    {
+      "poster": "https://www.dailynews.co.th/wp-content/uploads/2024/08/01-8.jpg",
+      "url": "intent://main.24playerhd.com/newplaylist/c43df236a46f7ab94f31577d/c43df236a46f7ab94f31577d.m3u8#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=MOVIETV;end"
+    },
+    {
+      "poster": "https://static1.cbrimages.com/wordpress/wp-content/uploads/2024/08/the-union-netflix.jpg",
+      "url": "intent://main.24playerhd.com/newplaylist/3b6046c367563f6a1e8d9ed9/3b6046c367563f6a1e8d9ed9.m3u8#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=MOVIETV;end"
+    },
+    {
+      "poster": "https://static1.cbrimages.com/wordpress/wp-content/uploads/2024/08/the-union-netflix.jpg",
+      "url": "intent://main.24playerhd.com/newplaylist/3b6046c367563f6a1e8d9ed9/3b6046c367563f6a1e8d9ed9.m3u8#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=MOVIETV;end"
+    },
+    {
+      "poster": "https://gg-th.com/videos/_YPTuniqid_5cd83e470ec9e4.33064023.jpg",
+      "url": "intent://main.24playerhd.com/m3u8/8e07318ef95bdbe2050b69da/8e07318ef95bdbe2050b69da168.m3u8#Intent;scheme=https;type=video/*;package=com.wiseplay;S.browser_fallback_url=market://details?id=com.wiseplay.ad;S.title=MOVIETV;end"
+    }
+  ]
+}
