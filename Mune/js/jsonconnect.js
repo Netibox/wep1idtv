863a384fc11a84d188a85a924cef9e1b7644f697
Netibox <aszsave123@gmail.com>
2024-09-20 13:45:37 +0700

    Update jsonconnect.js

--- Mune/js/jsonconnect.js
+++ Mune/js/jsonconnect.js
@@ -32,7 +32,7 @@ function(result) {
 });
 
 // jsonconnect sportlist
-$.getJSON('', 
+$.getJSON('https://cdn.statically.io/gh/Netibox/wep1idtv/main/Mune/js/sportlist.js', 
 function(result) {
   let sportlist = result.sportlist;
   //console.log(sportlist);
