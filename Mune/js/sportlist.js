62c227cf29dff9c8c20cfbe1dc61f07286ebe502
Netibox <aszsave123@gmail.com>
2024-09-20 08:42:53 +0700

    Add files via upload

---
+++ Mune/js/sportlist.js
@@ -0,0 +1,50 @@
+{
+  "sportlist": [
+    
+    {
+      "poster": "https://ais-s.ais-vidnt.com/ais/play/origin/VOD/Poster/24-6-11_P1.jpg",
+      "url": "3bbsport"
+    },
+       {
+      "poster": "https://ais-s.ais-vidnt.com/ais/play/origin/VOD/Poster/24-3-5_P15.jpg",
+      "url": "3bbasian"
+    },
+ 
+    {
+      "poster": "https://ais-s.ais-vidnt.com/ais/play/origin/VOD/Poster/24-6-11_P13.jpg",
+      "url": "hbo"
+    },
+    {
+      "poster": "https://ais-s.ais-vidnt.com/ais/play/origin/VOD/Poster/24-6-6_P13.jpg",
+      "url": "ch8"
+    },
+    {
+      "poster": "https://ais-s.ais-vidnt.com/ais/play/origin/VOD/Poster/24-6-9_P11.jpg",
+      "url": "3bbasian"
+    },
+    {
+      "poster": "https://ais-s.ais-vidnt.com/ais/play/origin/VOD/Poster/Thai-worldcup_11Jun.backdrop.jpg",
+      "url": "thairath"
+    },
+
+
+    {
+      "poster": "https://ais-s.ais-vidnt.com/ais/play/origin/VOD/Poster/24-6-5_P9.jpg",
+      "url": "pptv"
+    },
+    {
+      "poster": "https://ais-s.ais-vidnt.com/ais/play/origin/VOD/Poster/24-6-11_P11.jpg",
+      "url": "ch8"
+    },
+
+    {
+      "poster": "https://ais-s.ais-vidnt.com/ais/play/origin/VOD/Poster/24-3-27_P17.jpg",
+      "url": "hbohits"
+    },
+    
+    {
+      "poster": "https://ais-s.ais-vidnt.com/ais/play/origin/VOD/Poster/24-3-27_P11.jpg",
+      "url": "hbohits"
+    }
+  ]
+}
