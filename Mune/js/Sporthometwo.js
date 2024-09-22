60653882d0a619a6b89955c0ce28a2bf43dc4a43
Netibox <aszsave123@gmail.com>
2024-09-22 14:05:57 +0700

    Create Sporthometwo.js

---
+++ Mune/js/Sporthometwo.js
@@ -0,0 +1,18 @@
+function epl1() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://dolive.thaim3u.com/doo-tv-football-com/siamsport-1/playlist.m3u8?#Intent;package=com.wiseplay;S.referer=https://doo-tv.com/&userAgent=exo_player;S.title=PremierHD1;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+};
+function epl1two() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://www.xn--72czaud0ezbn4b8de.com/live/player/epl1#Intent;package=com.wiseplay;S.referer=https://www.xn--72czaud0ezbn4b8de.com/live/&userAgent=exo_player;S.title=PremierHD1;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+};
+function epl2() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://dolive.thaim3u.com/doo-tv-football-com/siamsport-2/playlist.m3u8?#Intent;package=com.wiseplay;S.referer=https://doo-tv.com/&userAgent=exo_player;S.title=NETBOX TV;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+};
+function epl2two() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://www.xn--72czaud0ezbn4b8de.com/live/player/epl2#Intent;package=com.wiseplay;S.referer=https://www.xn--72czaud0ezbn4b8de.com/live/&userAgent=exo_player;S.title=NETBOX TV;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+};
+function beinone() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://dookeela.live/live-tv/bein1#Intent;package=com.wiseplay;S.referer=https://dookeela.live/live-tv/&userAgent=exo_player;S.title=PremierHD3;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+};
+function beintwo() {
+  document.getElementById("objek").innerHTML = '<iframe src="intent://dookeela.live/live-tv/bein2#Intent;package=com.wiseplay;S.referer=https://dookeela.live/live-tv/&userAgent=exo_player;S.title=PremierHD4;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+};
