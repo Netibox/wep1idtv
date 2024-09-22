dc81ab12ff56b76469859ab41c006287c911ad15
Netibox <aszsave123@gmail.com>
2024-09-22 18:52:18 +0700

    Update tvonline.js

--- Mune/js/tvonline.js
+++ Mune/js/tvonline.js
@@ -36,7 +36,7 @@ function onehdtv() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://bcovlive-a.akamaihd.net/e11d0db446fe41fe9be61157033f9325/ap-southeast-1/6415628290001/playlist.m3u8#Intent;package=com.wiseplay;S.referer=https://bcovlive-a.akamaihd.net/&userAgent=exo_player;S.title=TVonline;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function pbs() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://thaipbs-live.cdn.byteark.com/live/playlist_480p/index.m3u8#Intent;package=com.wiseplay;S.referer=https:/thaipbs-live.cdn.byteark.com/&userAgent=exo_player;S.title=TVonline;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+  document.getElementById("objek").innerHTML = '<iframe src="intent://thaipbs-live.cdn.byteark.com/live/playlist_1080p/index.m3u8?checkedby:iptvcat.com#Intent;package=com.wiseplay;S.referer=https://thaipbs-live.cdn.byteark.com/live/&userAgent=exo_player;S.title=TVonline;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function voice() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://edge6a.v2h-cdn.com/v2h2/v2h2.stream/chunklist.m3u8#Intent;package=com.wiseplay;S.referer=https://edge6a.v2h-cdn.com/&userAgent=exo_player;S.title=TVonline;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
