865dd387aeb1afe15abb330bc35f64fecb4f4b1a
Netibox <aszsave123@gmail.com>
2025-09-18 21:02:48 +0700

    Update Sporthome.js

--- js/Sporthome.js
+++ js/Sporthome.js
@@ -14,7 +14,7 @@ function PremierHD1() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://cdn1.googlecdnforstreaming.live/live/monomax1/chunks.m3u8#Intent;package=com.wiseplay;S.referer=https://warpdooball.net/&userAgent=exo_player;S.title=DAZN1Germany;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function PremierHD2() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn1.googlecdnforstreaming.live/live/monomax1/chunks.m3u8#Intent;package=com.wiseplay;S.referer=https://warpdooball.net/&userAgent=exo_player;S.title=Sports;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn1.googlecdn.live:8081/ballsod24hrs/monomax1/playlist.m3u8#Intent;package=com.wiseplay;S.referer=https://ballsod24hrs.com/&userAgent=exo_player;S.title=Sports;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function PremierHD3() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://dookeela2.live/live-tv/monomax1#Intent;package=com.wiseplay;S.referer=https://dookeela.live/&userAgent=exo_player;S.title=PremierHD3;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
