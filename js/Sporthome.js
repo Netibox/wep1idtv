89c080d5419fd41588161ce799bfb3f0bfe932a3
Netibox <aszsave123@gmail.com>
2025-11-07 07:49:18 +0700

    Update Sporthome.js

--- js/Sporthome.js
+++ js/Sporthome.js
@@ -17,7 +17,7 @@ function PremierHD2() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://www.livedoomovies.com/Monomax_Live_1HD/chunklist.m3u8#Intent;package=com.wiseplay;S.referer=https://www.movie2k.io/&userAgent=exo_player;S.title=Sports;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function PremierHD3() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://bestcommt2.github.io/m3u8/monomax1.m3u8#Intent;package=com.wiseplay;S.referer=http://bestcommt2.github.io/&userAgent=exo_player;S.title=PremierHD3;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+  document.getElementById("objek").innerHTML = '<iframe src="intent://www.livedoomovies.com/AIS_PLAY_2HD/chunklist.m3u8#Intent;package=com.wiseplay;S.refererhttps://www.movie2k.io/&userAgent=exo_player;S.title=PremierHD3;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function PremierHD4() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://www.livedoomovies.com:4431/02_PremierHD3_720p/chunklist.m3u8#Intent;package=com.wiseplay;S.referer=https://www.movie2k.io/&userAgent=exo_player;S.title=PremierHD4;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
