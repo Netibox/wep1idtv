643baa3d91d88cb8486a0d6d86df6815b30735e5
Netibox <aszsave123@gmail.com>
2026-03-12 17:22:48 +0700

    Update Sporthome.js

--- Mune/js/Sporthome.js
+++ Mune/js/Sporthome.js
@@ -1,6 +1,6 @@
 
 function PremierHD1() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://github.com/cattviptv2605/sportthai/raw/refs/heads/main/MONOMAX1.m3u8#Intent;package=com.wiseplay;S.referer=https://github.com/cattviptv2605/sportthai/&userAgent=exo_player;S.title=PremierHD1;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+  document.getElementById("objek").innerHTML = '<iframe src="intent://github.com/cattv976/sportthai/raw/refs/heads/main/MONOMAX1.m3u8#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=PremierHD1;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function PremierHD11() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://www.livedoomovies.com:4431/Monomax_PL_1HD/playlist.m3u8#Intent;package=com.wiseplay;S.referer=https://www.movie2k.io/&userAgent=exo_player;S.title=PremierHD1;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
@@ -9,7 +9,7 @@ function PremierHD2() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://www.livedoomovies.com:4431/Monomax_Live_1HD/playlist.m3u8#Intent;package=com.wiseplay;S.referer=https://www.movie2k.io/&userAgent=exo_player;S.title=NETBOX TV;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function PremierHD3() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://www.livedoomovies.com:4431/02_PremierHD3_720p/chunklist.m3u8#Intent;package=com.wiseplay;S.referer=https://www.movie2k.io/&userAgent=exo_player;S.title=PremierHD3;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+  document.getElementById("objek").innerHTML = '<iframe src="intent://cdn2.googlecdnforstreaming.live/tubtoeball/tpf3/chunks.m3u8#Intent;package=com.wiseplay;S.referer=https://tubtoeball.net/&userAgent=exo_player;S.title=PremierHD3;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function PremierHD4() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://dookeela2.live/live-tv/tpf4#Intent;package=com.wiseplay;S.referer=https://dookeela.live/&userAgent=exo_player;S.title=PremierHD4;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
