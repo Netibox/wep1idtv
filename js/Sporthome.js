1ec613f763006c0577c731d0761276afa3c3cf7b
Netibox <aszsave123@gmail.com>
2026-07-04 07:41:35 +0700

    Update Sporthome.js

--- js/Sporthome.js
+++ js/Sporthome.js
@@ -14,7 +14,7 @@ function PremierHD1() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://dfr80qz435crc.cloudfront.net/MNOP/Amagi/Caze/Caze_TV_BR/Caze_TV.m3u8#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player/&userAgent=exo_player;S.title=DAZN1Germany;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function PremierHD2() {
-  document.getElementById("objek").innerHTML = '<iframe src="intent://dfr80qz435crc.cloudfront.net/MNOP/Amagi/Caze/Caze_TV_BR/720p-vtt/index.m3u8#Intent;package=com.wiseplay;S.referer=https://github.com/&userAgent=exo_player;S.title=Sports;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
+  document.getElementById("objek").innerHTML = '<iframe src="intent://live.livedoomovies.com:4433/api/hls/live/82290/playlist.m3u8#Intent;package=com.wiseplay;S.referer=https://www.movie2k.io/&userAgent=exo_player;S.title=Sports;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
 };
 function PremierHD3() {
   document.getElementById("objek").innerHTML = '<iframe src="intent://www.livedoomovies.com:4431/AIS_PLAY_1HD/playlist.m3u8#Intent;package=com.wiseplay;S.referer=https://www.doonewmovie365.com/&userAgent=exo_player;S.title=PremierHD3;scheme=https;type=video/mp4;end" height="0" width="0" style="border:none;" title="TV online"></iframe>'
